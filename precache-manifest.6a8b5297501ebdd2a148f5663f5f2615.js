ec45b7d436dcb2b22c5d69b7f6057ec6d1b133e4
vladimir <bajenovcpm433@yandex.ru>
2020-03-02 00:42:21 +0200

    Updates

---
+++ precache-manifest.6a8b5297501ebdd2a148f5663f5f2615.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "666ffd362f26eaf292adda210341ac55",
+    "url": "/battle-sea/index.html"
+  },
+  {
+    "revision": "a8ad8d588d3043c5bb89",
+    "url": "/battle-sea/static/css/main.710b67a3.chunk.css"
+  },
+  {
+    "revision": "87b1be4f4cb25a1d11aa",
+    "url": "/battle-sea/static/js/2.1111c284.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/battle-sea/static/js/2.1111c284.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8ad8d588d3043c5bb89",
+    "url": "/battle-sea/static/js/main.4a05dadb.chunk.js"
+  },
+  {
+    "revision": "c6f91dcbbc0d2926aa87",
+    "url": "/battle-sea/static/js/runtime-main.d708b9eb.js"
+  },
+  {
+    "revision": "5c54cb579437729dcc4df04548bd369b",
+    "url": "/battle-sea/static/media/android.5c54cb57.png"
+  },
+  {
+    "revision": "12da7eb72411980aaaaf78454a67309d",
+    "url": "/battle-sea/static/media/battle-31.12da7eb7.jpg"
+  },
+  {
+    "revision": "baadff8207f4098888aa2c0fc0dde07a",
+    "url": "/battle-sea/static/media/cubok.baadff82.png"
+  },
+  {
+    "revision": "f10c87e4bf96ff6f51deca77037f690d",
+    "url": "/battle-sea/static/media/game-img.f10c87e4.jpg"
+  },
+  {
+    "revision": "341db0a69f423fd6a62dd47cb27c43c2",
+    "url": "/battle-sea/static/media/login-img.341db0a6.png"
+  },
+  {
+    "revision": "8fd3a45da074919f48c1d2f444c96cc6",
+    "url": "/battle-sea/static/media/nav-btn2.8fd3a45d.jpg"
+  },
+  {
+    "revision": "c8d40b21e9fcec138f604a4f76da4fc7",
+    "url": "/battle-sea/static/media/navbtn-2.c8d40b21.jpg"
+  },
+  {
+    "revision": "83789f0af4902895a7f92232a1c5b5b7",
+    "url": "/battle-sea/static/media/placein.83789f0a.png"
+  },
+  {
+    "revision": "190f789db44abeb23b9a80ed2ec7e8b4",
+    "url": "/battle-sea/static/media/reset.190f789d.png"
+  },
+  {
+    "revision": "234d9f2e00e28f3071a4f958f921b7c2",
+    "url": "/battle-sea/static/media/rotate.234d9f2e.png"
+  },
+  {
+    "revision": "5cb3f243d6b5a8f407e38de82b72a3b4",
+    "url": "/battle-sea/static/media/sea-1.5cb3f243.jpg"
+  },
+  {
+    "revision": "64b72cb60a7d45bf83a0a2b38f5f8421",
+    "url": "/battle-sea/static/media/slide1.64b72cb6.jpg"
+  },
+  {
+    "revision": "1d00bb05883e99b0c1f5467ceebd9de9",
+    "url": "/battle-sea/static/media/slide2.1d00bb05.jpg"
+  },
+  {
+    "revision": "5d94e4b90e273c3a2fd1ed46b9420577",
+    "url": "/battle-sea/static/media/slide3.5d94e4b9.jpg"
+  },
+  {
+    "revision": "621b59ced31532c2980d2dd6caee52c9",
+    "url": "/battle-sea/static/media/stop-1.621b59ce.png"
+  },
+  {
+    "revision": "44263b0249802fdc0d994db4d68c781d",
+    "url": "/battle-sea/static/media/target.44263b02.png"
+  },
+  {
+    "revision": "7cc9bd54e0029b7b96f568f7d6a31bdf",
+    "url": "/battle-sea/static/media/user-male.7cc9bd54.png"
+  },
+  {
+    "revision": "d0d43991bc08f295910bdc0b3aabe764",
+    "url": "/battle-sea/static/media/winners-img.d0d43991.png"
+  }
+]);
